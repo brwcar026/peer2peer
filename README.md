74ff73ac9108f5b9d66e9e3a69d92131c68e4edb
Al <chrali007@myuct.ac.za>
2025-03-06 13:47:32 +0200

    added some comments and edited the README file

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # peer2peer
+
+# project name
+
+CSC3002F Networks Assignment 1 
+
+# description
+
+A program that uses a peer-to-peer system to share files to leechers from seeders through a TCP connection. 
+
+# set up
+
+1. Download the three files for the tracker, seeder and leecher. 
+2. Include any files you may wish to download in the folder of the program.
+3. Run the tracker first, then the seeder and finally the leecher. 
+4. Follow the prompts in the leecher program to dowload a file.
--- testSeeder.py
+++ testSeeder.py
@@ -1,7 +1,7 @@
 from socket import*
 
 def seeder():
-    serverPort = 12000
+    serverPort = 12000 #port number for leechers to connect to
     serverSocket = socket(AF_INET, SOCK_STREAM)
     serverSocket.bind(("0.0.0.0", serverPort))
     serverSocket.listen(1)
@@ -9,20 +9,20 @@ def seeder():
     
     while True:
         connectionSocket, addr = serverSocket.accept()
-        print("Connected to Leecher at: ", addr)
-        filename = connectionSocket.recv(1024).decode()
-        print("File to be sent: ", filename)
+        print("Connected to Leecher at: ", addr) # provides information about the leecher currently connected to the seeder
+        filename = connectionSocket.recv(1024).decode() # receives the requested file name from the leecher
+        print("File to be sent: ", filename) #verifies the correct file to be downloaded
         
         try:
             with open(filename, "rb") as file:
                 while chunk := file.read(1024):
-                    connectionSocket.send(chunk)
-            print("File sent successfully!")
+                    connectionSocket.send(chunk) # sends the file to the leecher in chunks of size 1024 bytes
+            print("File sent successfully!") #confirmation message from the seeder
         except FileNotFoundError:
-            print("File not found!")
+            print("File not found!") # informs leecher the file could not be found
             connectionSocket.send(b"ERROR: File not found")
         
-        connectionSocket.close()
+        connectionSocket.close() #closes the socket
 
 if __name__ == "__main__":
-    seeder()
+    seeder() #runs the leecher class
