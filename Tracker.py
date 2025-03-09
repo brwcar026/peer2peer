34d8bebf1845974d196e62a978f43eb208fe16f7
adamsnia <qaaniadams10@gmail.com>
2025-03-09 13:09:58 +0200

    Updated Tracker.py

--- Tracker.py
+++ Tracker.py
@@ -1,6 +1,66 @@
 from socket import *
+import time
+import threading
 
+# Dictionary to store seeder information
+# Key: file name, Value: list each seeder info (seeder_ip, seeder_port, last_active_time)
 seeders = {}
+
+def trackClient(message, clientAddress, udpSocket):
+    modifiedMessage = message.decode().upper()
+    #print(f"Received message: {modifiedMessage}")
+    
+    splitProtocol = decodedMessage.split()
+    
+    if splitProtocol[0] == "REGISTER":
+        file = splitProtocol[1]
+        seederPort = splitProtocol[2]
+        seederIPAdd = clientAddress[0]
+    
+        if file not in seeders:
+            seeders[file] = []
+        seederInfo = {'ip': seederIPAdd, 'port':seederPort, 'Latest Active Time': time.time()}
+        seeders[file].append(seederInfo)
+        #print(f"Seeder {seeder_ip}:{seeder_port} registered for file {file_name}")
+        udpSocket.sendto("REGISTERED", clientAddress)# send a confirmation message to seeder
+    
+    elif splitProtocol[0] == "QUERY":
+        file = splitProtocol[1]
+        if file in seeders:
+            '''activeSeeders = []
+            for ip in seeders[file]:
+                t = 
+                if time.time() - t < 60:
+                    active_seeders.append((ip, port, t))
+            seeders[file_name] = active_seeders'''
+            
+            #seeders[file_name] = [(ip, port, t) for (ip, port, t) in seeders[file_name] if time.time() - t < 60]
+            
+            activeSeeders = []
+            for seeder in seeders[file]:
+                if (time.time() - seeder['Latest Active Time']) < 60:
+                    activeSeeders.append(seeder)
+            if activeSeeders == []:
+                # No active seeders found
+                udpSocket.sendto("No Active Seeders", clientAddress)
+                print(f"No active seeders found for file {file}")
+            else:
+                # Format: "ip1:port1 ip2:port2 ..."
+                seederParts = []                
+                for seeder in activeSeeders:
+                    seederParts.append(f"{seeder['ip']}:{seeder['port']}")
+                seederList = " ".join(seederParts)
+                udpSocket.sendto(seederList.encode(), clientAddress)
+                print(f"Sent active seeders for file {file} to {clientAddress}: {seederList}") 
+                
+                '''seederList = ",".join([f"{seeder['ip']}:{seeder['port']}" for seeder in activeSeeders])
+                udpSocket.sendto(seederList.encode(), clientAddress)
+                print(f"Sent active seeders for file {file} to {clientAddress}: {seederList}")'''               
+                
+        else:
+            # File not found in seeders
+            udpSocket.sendto("File Not Found", clientAddress)
+            print(f"File {file} not found")   
 
 def tracker():
     udp_trackerPort = 12002
